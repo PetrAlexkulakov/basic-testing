6031db42688d4123d29be54c14ec5d7e2874a75f
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-07-02 13:02:37 +0400

    feat: create eighth file tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const list = [1, 2, 3];
+const result = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: null,
+        next: null,
+      },
+    },
+  },
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const linkedList = generateLinkedList(list);
+
+    expect(linkedList).toStrictEqual(result);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const linkedList = generateLinkedList(list);
+
+    expect(linkedList).toMatchSnapshot();
   });
 });
